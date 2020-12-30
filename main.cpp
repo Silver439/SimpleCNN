435d26cca06b111daea1760a86dc4fb337f98615
Silver439 <72134457+Silver439@users.noreply.github.com>
2020-12-30 21:12:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,20 @@ extern conv_param conv_params[3];
 extern fc_param fc_params[1];
 
 float* mat_1d(Mat&);
-float* conv_relu(int, int, int, float*, conv_param);
+float* conv_relu(int, int, int, float*, conv_param&);
 float* maxpool(int, int, int, float*);
 
 int main()
 {
-	Mat img = imread("6.jpg");
+	Mat img = imread("2.jpg");
 	float* res = mat_1d(img);
+
 	float* p0 = conv_relu(3, 128, 128, res, conv_params[0]);
 	float* p1 = maxpool(16, 64, 64, p0);
+
 	float* p2 = conv_relu(16, 32, 32, p1, conv_params[1]);
 	float* p3 = maxpool(32, 32, 32, p2);
+
 	float* p4 = conv_relu(32, 16, 16, p3, conv_params[2]);
 
 	float* p5 = new float[2]{ 0 };
@@ -30,12 +33,8 @@ int main()
 	float conf1, conf2;
 	conf1 = exp(p5[0]) / (exp(p5[0]) + exp(p5[1]));
 	conf2 = exp(p5[1]) / (exp(p5[0]) + exp(p5[1]));
-	if (conf1 > conf2) {
-		cout << "对不起，你有" << conf1 * 100 << "%" << "的概率不是人。" << endl;
-	}
-	else {
-		cout << "恭喜！你有" << conf2 * 100 << "%" << "的概率是个人呢！" << endl;
-	}
+	cout << "backgrond:" << conf1 << endl;
+	cout << "face:" << conf2 << endl;
 }
 
 float* mat_1d(Mat& img)
@@ -66,12 +65,15 @@ float* mat_1d(Mat& img)
 	return res;
 }
 
-float* conv_relu(int cha, int row, int col, float* inp, conv_param cp)
+float* conv_relu(int cha, int row, int col, float* inp, conv_param &cp)
 {
-	int n = 0;
+	int num = 0;
 	int o_cha = cp.out_channels;
-	int sk = cp.kernel_size;
+	int i_cha = cp.in_channels;
+	int ks = cp.kernel_size;
 	int st = cp.stride;
+	if (cp.pad == 1)
+	{
 		float* pd_inp = new float[cha * (row * col + 2 * col + 2 * row + 4)]{ 0 };
 		for (int i = 1; i <= cha; i++)
 		{
@@ -79,7 +81,7 @@ float* conv_relu(int cha, int row, int col, float* inp, conv_param cp)
 			{
 				for (int k = 1; k <= col; k++)
 				{
-					pd_inp[(i - 1) * (row + 2) * (col + 2) + j * (col + 2) + k] = inp[n++];
+					pd_inp[(i - 1) * (row + 2) * (col + 2) + j * (col + 2) + k] = inp[num++];
 				}
 			}
 		}
@@ -87,16 +89,17 @@ float* conv_relu(int cha, int row, int col, float* inp, conv_param cp)
 		float* res = new float[o_cha * row * col / st / st]{ 0 };
 		for (int i = 0; i < o_cha; i++)
 		{
-			int forword_num = i * sk * sk * cp.in_channels;
+			int forword_num = i * ks * ks * i_cha;
 			for (int j = 1; j <= cha; j++)
 			{
-				int fn = forword_num + (j - 1) * sk * sk;
+				int fn = forword_num + (j - 1) * ks * ks;
 				for (int k = 1; k <= row; k += cp.stride)
 				{
 					for (int l = 1; l <= col; l += cp.stride)
 					{
 						int fn2 = (j - 1) * (row + 2) * (col + 2) + k * (col + 2) + l;
-						res[i * row * col / st / st + (k - 1) * col / st / st + (l - 1) / st] += pd_inp[fn2] * cp.p_weight[fn + 4] +
+						res[i * row * col / st / st + (k - 1) * col / st / st + (l - 1) / st] += 
+							pd_inp[fn2] * cp.p_weight[fn + 4] +
 							pd_inp[fn2 - 1] * cp.p_weight[fn + 3] +
 							pd_inp[fn2 + 1] * cp.p_weight[fn + 5] +
 							pd_inp[fn2 - col - 2] * cp.p_weight[fn + 1] +
@@ -124,7 +127,52 @@ float* conv_relu(int cha, int row, int col, float* inp, conv_param cp)
 			}
 		}
 		return res;
-	
+	}
+	else
+	{
+		float* res = new float[o_cha * (row-2) * (col-2) / st / st]{ 0 };
+		for (int i = 0; i < o_cha; i++)
+		{
+			int forword_num = i * ks * ks * i_cha;
+			for (int j = 1; j <= cha; j++)
+			{
+				int fn = forword_num + (j - 1) * ks * ks;
+				for (int k = 1; k <= row-2; k += cp.stride)
+				{
+					for (int l = 1; l <= col-2; l += cp.stride)
+					{
+						int fn2 = (j-1) * (row) * (col) + k * (col) + l;
+						res[i * (row-2) * (col-2) / st / st + (k - 1) * (col-2) / st / st + (l - 1) / st] +=
+							inp[fn2] * cp.p_weight[fn + 4] +
+							inp[fn2 - 1] * cp.p_weight[fn + 3] +
+							inp[fn2 + 1] * cp.p_weight[fn + 5] +
+							inp[fn2 - col - 2] * cp.p_weight[fn + 1] +
+							inp[fn2 + col + 2] * cp.p_weight[fn + 7] +
+							inp[fn2 - 1 - col - 2] * cp.p_weight[fn] +
+							inp[fn2 - 1 + col + 2] * cp.p_weight[fn + 6] +
+							inp[fn2 + 1 - col - 2] * cp.p_weight[fn + 2] +
+							inp[fn2 + 1 + col + 2] * cp.p_weight[fn + 8];
+					}
+				}
+			}
+		}
+		int total = o_cha * (row-2) * (col-2) / st / st;
+		for (int i = 0; i < o_cha; i++)
+		{
+			for (int j = 0; j < (row-2) * (col-2) / st / st; j++)
+			{
+				res[i * (row-2) * (col-2) / st / st + j] += cp.p_bias[i];
+			}
+		}
+		for (int i = 0; i < total; i++)
+		{
+			if (res[i] < 0) {
+				res[i] = 0;
+			}
+		}
+		delete[] inp;
+		return res;
+	}
 }
 
 float* maxpool(int cha, int row, int col, float* inp)
