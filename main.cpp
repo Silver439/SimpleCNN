14aad0a7d6ebc4e3827a5ba156f42673d214e912
Silver439 <72134457+Silver439@users.noreply.github.com>
2021-01-02 12:32:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,15 @@ float* mat_1d(Mat&);
 float* conv_relu(int, int, int, float*, conv_param&);
 float* maxpool(int, int, int, float*);
 
+float* quick_conv_relu(int cha, int row, int col, float* inp, conv_param& cp);
+void matrixproduct(float* C, float* A,int r,int col,int c,float* B);
+
 int main()
 {
-	Mat img = imread("2.jpg");
+	Mat img = imread("10.jpg");
 	float* res = mat_1d(img);
+	clock_t start, End;
+	start = clock();
 
 	float* p0 = conv_relu(3, 128, 128, res, conv_params[0]);
 	float* p1 = maxpool(16, 64, 64, p0);
@@ -35,6 +40,9 @@ int main()
 	conf2 = exp(p5[1]) / (exp(p5[0]) + exp(p5[1]));
 	cout << "backgrond:" << conf1 << endl;
 	cout << "face:" << conf2 << endl;
+	End = clock();
+	double endtime = (double)(End - start) / CLOCKS_PER_SEC;
+	cout << "Total time:" << endtime * 1000 << "ms" << endl;
 }
 
 float* mat_1d(Mat& img)
@@ -60,6 +68,82 @@ float* mat_1d(Mat& img)
 			res[2 * n + i * col + j + 1] = (float)p[3 * j + 3] / (float)255;
 			res[2 * n + i * col + j + 2] = (float)p[3 * j + 6] / (float)255;
 			res[2 * n + i * col + j + 3] = (float)p[3 * j + 9] / (float)255;
+		}
+	}
+	return res;
+}
+
+float* quick_conv_relu(int cha, int row, int col, float* inp, conv_param& cp)
+{
+	int num = 0;
+	int o_cha = cp.out_channels;
+	int i_cha = cp.in_channels;
+	int ks = cp.kernel_size;
+	int st = cp.stride;
+	float* res = new float[row / st * col / st * o_cha]{0};
+	float* pd_inp = new float[cha * (row * col + 2 * col + 2 * row + 4)]{ 0 };
+	for (int i = 1; i <= cha; i++)
+	{
+		for (int j = 1; j <= row; j++)
+		{
+			for (int k = 1; k <= col; k++)
+			{
+				pd_inp[(i - 1) * (row + 2) * (col + 2) + j * (col + 2) + k] = inp[num++];
+			}
+		}
+	}
+	delete[] inp;
+	int loop = 0;
+	float* m_matrix;
+	float* k_matrix;
+	do
+	{
+		int n = 0;
+		m_matrix = new float[row / st * col / st * ks * ks]{ 0 };
+		for (int k = 1; k <= row; k += st)
+		{
+			for (int l = 1; l <= col; l += st)
+			{
+				int fn2 = (loop) * (row + 2) * (col + 2) + k * (col + 2) + l;
+				m_matrix[n] = pd_inp[fn2-col-3];
+				m_matrix[n+1] = pd_inp[fn2-col-2];
+				m_matrix[n+2] = pd_inp[fn2-col-1];
+				m_matrix[n+3] = pd_inp[fn2-1];
+				m_matrix[n+4] = pd_inp[fn2];
+				m_matrix[n+5] = pd_inp[fn2 + 1];
+				m_matrix[n+6] = pd_inp[fn2+col+1];
+				m_matrix[n+7] = pd_inp[fn2+col+2];
+				m_matrix[n+8] = pd_inp[fn2+col+3];
+				n += 9;
+		    }
+	    }
+		int n2 = 0;
+		k_matrix = new float[o_cha * ks * ks]{0};
+		for (int i = 0; i < o_cha; i++)
+		{
+			int fn = i * i_cha * ks * ks + loop * ks * ks;
+			for (int j = 0; j < ks*ks; j++)
+			{
+				k_matrix[n2++] = cp.p_weight[fn + j];
+			}
+		}
+		matrixproduct(res, m_matrix, row * col / st / st, ks * ks, o_cha, k_matrix);
+		loop += 1;
+		delete[] m_matrix;
+		delete[] k_matrix;
+	} while (loop < cha);
+	int total = o_cha * row * col / st / st;
+	for (int i = 0; i < o_cha; i++)
+	{
+		for (int j = 0; j < row * col / st / st; j++)
+		{
+			res[i * row * col / st / st + j] += cp.p_bias[i];
+		}
+	}
+	for (int i = 0; i < total; i++)
+	{
+		if (res[i] < 0) {
+			res[i] = 0;
 		}
 	}
 	return res;
@@ -194,4 +278,33 @@ float* maxpool(int cha, int row, int col, float* inp)
 	}
 	delete[] inp;
 	return res;
+}
+
+void matrixproduct(float* C, float* A, int r, int col, int c, float* B)
+{
+	for (int i = 1; i <= c; i+=4)
+	{
+		for (int j = 1; j <= r; j+=4)
+		{
+			for (int k = 0; k < col; k++)
+			{
+				C[(i - 1) * r + j - 1] += A[9 * (j - 1) + k] * B[9 * (i - 1) + k];
+				C[(i - 1) * r + j] += A[9 * (j) + k] * B[9 * (i - 1) + k];
+				C[(i - 1) * r + j + 1] += A[9 * (j + 1) + k] * B[9 * (i - 1) + k];
+				C[(i - 1) * r + j + 2] += A[9 * (j + 2) + k] * B[9 * (i - 1) + k];
+				C[(i) * r + j - 1] += A[9 * (j - 1) + k] * B[9 * (i) + k];
+				C[(i) * r + j] += A[9 * (j)+k] * B[9 * (i) + k];
+				C[(i) * r + j + 1] += A[9 * (j + 1) + k] * B[9 * (i) + k];
+				C[(i) * r + j + 2] += A[9 * (j + 2) + k] * B[9 * (i) + k];
+				C[(i + 1) * r + j - 1] += A[9 * (j - 1) + k] * B[9 * (i + 1) + k];
+				C[(i + 1) * r + j] += A[9 * (j)+k] * B[9 * (i + 1) + k];
+				C[(i + 1) * r + j + 1] += A[9 * (j + 1) + k] * B[9 * (i + 1) + k];
+				C[(i + 1) * r + j + 2] += A[9 * (j + 2) + k] * B[9 * (i + 1) + k];
+				C[(i + 2) * r + j - 1] += A[9 * (j - 1) + k] * B[9 * (i + 2) + k];
+				C[(i + 2) * r + j] += A[9 * (j)+k] * B[9 * (i + 2) + k];
+				C[(i + 2) * r + j + 1] += A[9 * (j + 1) + k] * B[9 * (i + 2) + k];
+				C[(i + 2) * r + j + 2] += A[9 * (j + 2) + k] * B[9 * (i + 2) + k];
+			}
+		}
+	}
 }
